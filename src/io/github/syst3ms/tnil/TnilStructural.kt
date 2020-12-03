2d74cf34d64cc547bdad20abafd8ffe621cdf502
ngoriyasjil <74504155+ngoriyasjil@users.noreply.github.com>
2020-12-03 18:58:03 +0200

    added Category interface
    Got distracted

--- src/io/github/syst3ms/tnil/TnilStructural.kt
+++ src/io/github/syst3ms/tnil/TnilStructural.kt
@@ -4,13 +4,25 @@ interface Precision {
     fun toString(precision: Int, ignoreDefault: Boolean = false): String
 }
 
-class Slot(private vararg val values: Precision?) : Precision {
+interface Category : Precision {
+    val ordinal: Int
+    val name: String
+    val short: String
+
+    override fun toString(precision: Int, ignoreDefault: Boolean) = when {
+        ignoreDefault && this.ordinal == 0 -> ""
+        precision >= 2 -> this.name.toLowerCase().replace("_", " ")
+        else -> short
+    }
+}
+
+
+class Slot(private vararg val values: Precision) : Precision {
 
     var stemUsed = false
 
     override fun toString(precision: Int, ignoreDefault: Boolean): String {
         return values
-            .filterNotNull()
             .map {
                 val gloss = it.toString(precision, ignoreDefault)
                 if (stemUsed && it is Stem) "__${gloss}__" else gloss
