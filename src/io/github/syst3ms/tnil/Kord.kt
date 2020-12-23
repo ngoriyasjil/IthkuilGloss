3a1e79c9a2914f931badb2a7f9ee01d57c8d8665
ngoriyasjil <74504155+ngoriyasjil@users.noreply.github.com>
2020-12-23 11:17:51 +0200

    Up and running with same functionality

--- src/io/github/syst3ms/tnil/Kord.kt
+++ src/io/github/syst3ms/tnil/Kord.kt
@@ -1,10 +1,50 @@
 package io.github.syst3ms.tnil
 
 import dev.kord.core.*
+import dev.kord.core.entity.Message
+import dev.kord.core.event.message.MessageCreateEvent
+
+import java.io.File
 
 suspend fun main() {
-    val token = tokenFile.readLines()[0]
+    val token = File("./resources/token.txt").readLines()[0]
     val kord = Kord(token)
 
-    kord.login()
+    kord.on<MessageCreateEvent> {
+        if (!message.content.startsWith("?") || message.author?.isBot != false) return@on
+
+        if (message.content == "?help") {
+            sendHelp(message)
+            return@on
+        }
+
+        val response = respond(message.content) ?: return@on
+
+        val messages = response.splitMessages()
+
+        messages.forEach {
+            message.channel.createMessage(it)
+        }
+
+    }
+
+    loadResourcesOnline()
+    kord.login {
+        playing("?help for info")
+    }
+}
+
+ suspend fun sendHelp(message : Message) {
+
+    val helpee = message.author ?: return
+    val dmChannel = helpee.getDmChannel()
+
+    val helpMessages = File("./resources/help.md")
+        .readText()
+        .splitMessages()
+
+    helpMessages.forEach { dmChannel.createMessage(it) }
+
+    message.channel.createMessage("Help sent your way, ${helpee.mention}!")
+
 }
