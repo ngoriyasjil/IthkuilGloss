656ec4f1d58158327653058ffa70a2aabb699ac4
ngoriyasjil <74504155+ngoriyasjil@users.noreply.github.com>
2021-04-15 23:54:14 +0300

    Added a few tentative tests

--- test/SlotTests.kt
+++ test/SlotTests.kt
@@ -1,15 +1,35 @@
 package ithkuil.iv.gloss.test
 
-import ithkuil.iv.gloss.GlossOptions
-import ithkuil.iv.gloss.parseCa
+import ithkuil.iv.gloss.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
-
-infix fun String.isCaOf(gloss: String) = assertEquals(gloss, parseCa(this)?.toString(GlossOptions()), this)
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNotNull
 
 class SlotTests {
+
     @Test
-    fun caTest() {
+    fun `Cc parses only for values in CC_CONSONANTS`() {
+        CC_CONSONANTS.forEach { it isNotCcOf (null to null) }
+        (CN_CONSONANTS - CC_CONSONANTS).forEach { it isCcOf (null to null) }
+    }
+
+    @Test
+    fun ccExamples() {
+        "hl" isCcOf (Concatenation.TYPE_ONE to Shortcut.W_SHORTCUT)
+        "y" isCcOf (null to Shortcut.Y_SHORTCUT)
+        "hw" isCcOf (Concatenation.TYPE_TWO to null)
+    }
+
+    @Test
+    fun `Special Vv parses for all values in SPECIAL_VV_VOWELS`() {
+        SPECIAL_VV_VOWELS.forEach {
+            assertNotNull(parseSpecialVv(it, null), it)
+        }
+    }
+
+    @Test
+    fun caExamples() {
         "l" isCaOf ""
         "s" isCaOf "DPX"
         "nļ" isCaOf "ASO"
@@ -19,4 +39,19 @@ class SlotTests {
         "zḑ" isCaOf "MFS.DPL.A.RPV"
         "nx" isCaOf "MSC.GRA.N.RPV"
     }
+}
+
+infix fun String.isCcOf(pair: Pair<Concatenation?, Shortcut?>) {
+    val gloss = parseCc(this)
+    assertEquals(pair, gloss, this)
+}
+
+infix fun String.isNotCcOf(illegal: Pair<Concatenation?, Shortcut?>) {
+    val gloss = parseCc(this)
+    assertNotEquals(illegal, gloss, this)
+}
+
+infix fun String.isCaOf(expected: String) {
+    val gloss = parseCa(this)?.toString(GlossOptions())
+    assertEquals(expected, gloss, this)
 }
